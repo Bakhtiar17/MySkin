5d5c7c34346ede8930562d2f5fde15ed5850b4af
Muhammad_Eka_Bakhtiar <bakhtiar@student.telkomuniversity.ac.id>
2021-10-23 11:00:50 +0700

    Menambahkan navigasi komponen

--- app/src/main/java/org/d3ifcool/MySkin/MainActivity.kt
+++ app/src/main/java/org/d3ifcool/MySkin/MainActivity.kt
@@ -2,15 +2,23 @@ package org.d3ifcool.MySkin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import org.d3ifcool.MySkin.databinding.ActivityMainBinding
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        navController = findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp()
     }
 }
---
+++ app/src/main/java/org/d3ifcool/MySkin/ui/AwalFragment.kt
@@ -0,0 +1,20 @@
+package org.d3ifcool.MySkin.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.d3ifcool.MySkin.R
+import org.d3ifcool.MySkin.databinding.FragmentAwalBinding
+
+class AwalFragment : Fragment(R.layout.fragment_awal) {
+    private lateinit var binding: FragmentAwalBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentAwalBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
+
+}
