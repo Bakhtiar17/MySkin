eabfedf902b56a5f033e53f493271d13cf1315a2
Muhammad_Eka_Bakhtiar <bakhtiar@student.telkomuniversity.ac.id>
2021-10-27 20:51:35 +0700

    Menambahkan tampilan Tips & Tricks

---
+++ app/src/main/java/org/d3ifcool/MySkin/tips/TipsAndTricks.kt
@@ -0,0 +1,12 @@
+package org.d3ifcool.MySkin.tips
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.d3ifcool.MySkin.R
+
+class TipsAndTricks : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tips_and_tricks)
+    }
+}
--- app/src/main/java/org/d3ifcool/MySkin/ui/AwalFragment.kt
+++ app/src/main/java/org/d3ifcool/MySkin/ui/AwalFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import org.d3ifcool.MySkin.R
 import org.d3ifcool.MySkin.databinding.FragmentAwalBinding
 
@@ -14,6 +15,11 @@ class AwalFragment : Fragment(R.layout.fragment_awal) {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentAwalBinding.inflate(layoutInflater, container, false)
 
+        binding.button2.setOnClickListener { view: View -> view.findNavController().navigate(
+            R.id.action_awalFragment_to_tipsAndTricks
+        )
+
+        }
         return binding.root
     }
 
