f4751d88d70758f205163184da6a37125209a486
dennaputri <dennaputri@student.telkomuniversity.ac.id>
2021-10-28 11:22:53 +0700

    laman permasalahan kulit

--- app/src/main/java/org/d3ifcool/MySkin/ui/masalah_kulit.kt
+++ app/src/main/java/org/d3ifcool/MySkin/ui/masalah_kulit.kt
@@ -1,25 +1,26 @@
 package org.d3ifcool.MySkin.ui
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
+import androidx.navigation.NavController
 import org.d3ifcool.MySkin.R
 import org.d3ifcool.MySkin.databinding.FragmentAwalBinding
+import org.d3ifcool.MySkin.databinding.MasalahKulitBinding
 
-class AwalFragment : Fragment(R.layout.fragment_awal) {
-    private lateinit var binding: FragmentAwalBinding
+class masalah_kulit : Fragment(R.layout.masalah_kulit){
 
+    private lateinit var binding: MasalahKulitBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = FragmentAwalBinding.inflate(layoutInflater, container, false)
 
-        binding.button.setOnClickListener { view: View -> view.findNavController().navigate(
-            R.id.action_awalFragment_to_masalah_kulit
-        ) }
-
+        binding = MasalahKulitBinding.inflate(layoutInflater,container,false)
         return binding.root
-    }
 
+    }
 }
+
+
