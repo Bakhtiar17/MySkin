834a062cfe793659b9d91dd882c0c7cbd60467c2
Muhammad_Eka_Bakhtiar <bakhtiar@student.telkomuniversity.ac.id>
2021-12-02 14:43:24 +0700

    flexible UI

--- app/src/main/java/org/d3ifcool/MySkin/tips/TipsAndTriks.kt
+++ app/src/main/java/org/d3ifcool/MySkin/tips/TipsAndTriks.kt
@@ -1,7 +1,15 @@
 package org.d3ifcool.MySkin.tips
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import org.d3ifcool.MySkin.R
 
 class TipsAndTriks: Fragment(R.layout.tips_and_tricks) {
+
+    private var isTablet: Boolean = false
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        isTablet = resources.getBoolean(R.bool.isTablet)
+    }
 }
